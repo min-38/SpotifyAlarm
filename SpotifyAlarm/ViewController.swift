7a9cfd9266a11dda1038bf982e2fa8eab2b411eb
min-38 <mindino03@gmail.com>
2024-01-16 23:47:34 +0900

    #1-요일 선택 버튼 기능 작업
    - 클릭 시 0, 1로 on/off 할 수 있도록 함
    - on/off 시 스타일 주지 않음
    - 시작 버튼 클릭 시 데이터 모두 가져와서 저장하는 기능 구현 필요
    - 스타일링 필요

--- SpotifyAlarm/ViewController.swift
+++ SpotifyAlarm/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var setAlarmBtn: UIButton!
+    @IBOutlet weak var DaysStack: UIStackView!
     @IBOutlet weak var timePicker: UIDatePicker!
+    
+    var time : [String : Int] = ["hour": 0, "minute": 0];
+    var days : [String : Int] = ["Monday" : 0, "Tuesday" : 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0];
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,21 +26,65 @@ class ViewController: UIViewController {
         setAlarmBtn.addTarget(self, action: #selector(setAlarm), for: .touchUpInside)
     }
     
-    @IBAction func setAlarm(_ sender: UIButton) {
-        let time = getTime()
-        print(time["hour"]!)
-        print(time["minute"]!)
+    @IBAction func setDays(_ sender: Any) {
+        guard let day = (sender as? UIButton)?.tag else {
+            return
+        }
+        
+        var s : String = "Monday"
+        
+        switch day {
+        case 0:
+            s = "Monday"
+        case 1:
+            s = "Tuesday"
+        case 2:
+            s = "Wednesday"
+        case 3:
+            s = "Thursday"
+        case 4:
+            s = "Friday"
+        case 5:
+            s = "Saturday"
+        case 6:
+            s = "Sunday"
+        default:
+            s = "Monday"
+        }
+        
+        if days[s] == 0 {
+            days[s] = 1
+            (sender as! UIButton).backgroundColor = .red
+        } else {
+            days[s] = 0
+            (sender as! UIButton).backgroundColor = .blue
+        }
     }
     
-    func getTime() -> Dictionary<String, Int> {
+    @IBAction func setAlarm(_ sender: UIButton) {
+        getTime()
+        getDays()
+//        print(time["hour"]!)
+//        print(time["minute"]!)
+    }
+    
+    @IBAction func getTime() {
         let targetTime = timePicker.date
 
         let components = Calendar.current.dateComponents([.hour, .minute], from: targetTime)
         let hour = components.hour!
         let minute = components.minute!
         
-        let dic : [String : Int] = ["hour" : hour, "minute" : minute]
+        time = ["hour" : hour, "minute" : minute]
+    }
+    
+    @IBAction func getDays() {
+        let targetTime = timePicker.date
+
+        let components = Calendar.current.dateComponents([.hour, .minute], from: targetTime)
+        let hour = components.hour!
+        let minute = components.minute!
         
-        return dic
+        time = ["hour" : hour, "minute" : minute]
     }
 }
