38a1bc36aca8d7d062dcc5e5c09c65dc7a9fd3c0
min-38 <mindino03@gmail.com>
2024-01-16 20:46:40 +0900

    #1-알림 생성 뷰 작업
    - 시간 DatePicker, 요일 버튼 뷰 작업
    - 시작 버튼 클릭 시 우선 시, 분만 가져옴
    - 버튼 작업 필요

--- SpotifyAlarm/ViewController.swift
+++ SpotifyAlarm/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var setAlarmBtn: UIButton!
+    @IBOutlet weak var timePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        timePicker.timeZone = TimeZone.init(identifier: "UTC")
+        timePicker.datePickerMode = .time
+
+        setAlarmBtn.addTarget(self, action: #selector(setAlarm), for: .touchUpInside)
     }
+    
+    @IBAction func setAlarm(_ sender: UIButton) {
+        let time = getTime()
+        print(time["hour"]!)
+        print(time["minute"]!)
+    }
+    
+    func getTime() -> Dictionary<String, Int> {
+        let targetTime = timePicker.date
 
-
+        let components = Calendar.current.dateComponents([.hour, .minute], from: targetTime)
+        let hour = components.hour!
+        let minute = components.minute!
+        
+        let dic : [String : Int] = ["hour" : hour, "minute" : minute]
+        
+        return dic
+    }
 }
-
